73113e204838ea34ac4da630f40d3ee87988f1e1
h-waqar <hamzawaqar556@gmail.com>
2025-10-30 07:58:25 +0500

    Sidebar ::before visual bug fix

--- source/plugin-pages/side-bar.php
+++ source/plugin-pages/side-bar.php
@@ -1,25 +1,25 @@
 <!-- Side Bar -->
 
 <section id="sidebar_ProgressBar">
-    <div class="cs-container overflow-hidden">
-        <nav class="bs-navmenu">
-            <ul>
-                <li>
-                    <a href="#" id="bsNavIntroduction" class="bs-active">Select Medical Protection</a>
-                </li>
-                <li>
-                    <a href="#" id="bsNavBasicInfo">Personal Information</a>
-                </li>
-                <li>
-                    <a href="#" id="bsNavYourGp">Trip Information</a>
-                </li>
-                <li>
-                    <a href="#" id="bsNavReviewSubmit">Summary</a>
-                </li>
-                <li>
-                    <a href="#" id="bsNavCheckout" class="bs-last">Checkout</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
+  <div class="cs-container overflow-hidden">
+    <nav class="bs-navmenu">
+      <ul>
+        <li>
+          <a href="#" id="bsNavIntroduction" class="bs-active">Medical Protection</a>
+        </li>
+        <li>
+          <a href="#" id="bsNavBasicInfo">Personal Information</a>
+        </li>
+        <li>
+          <a href="#" id="bsNavYourGp">Trip Information</a>
+        </li>
+        <li>
+          <a href="#" id="bsNavReviewSubmit">Summary</a>
+        </li>
+        <li>
+          <a href="#" id="bsNavCheckout" class="bs-last">Checkout</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </section>
